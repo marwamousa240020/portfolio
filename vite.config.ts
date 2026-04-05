679a9afb2c0edd97118584c2555900086e74b040
marwamousa240020 <240020@buas.nl>
2026-04-05 21:45:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfoilo/",
+  base: "/portfoilo/", // 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
